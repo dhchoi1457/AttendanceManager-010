dea3c577943b3488b6e7674cc87b19d6f45cb6a9
dohyun <alvin1785@gmail.com>
2025-09-22 14:57:16 +0900

    [refactor] 불필요한 코드 제거

--- mission2/src/main.py
+++ mission2/src/main.py
@@ -1,27 +1,5 @@
 from collections import defaultdict
-
 from mission2.src.manager import AttendanceManager
-
-DAYS = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
-DAY_POINTS = {
-    "monday": 1, "tuesday": 1,
-    "wednesday": 3,
-    "thursday": 1, "friday": 1,
-    "saturday": 2, "sunday": 2
-}
-BONUS_THRESHOLD = 10
-BONUS_POINTS = 10
-GRADE_THRESHOLDS = {"GOLD": 50, "SILVER": 30}
-
-user_ids = {}
-user_count = 0
-names = {}
-attendance = defaultdict(lambda: defaultdict(int))
-points = defaultdict(int)
-grades = {}
-wednesday_count = defaultdict(int)
-weekend_count = defaultdict(int)
-
 
 def process_attendance_file(filename="../data/attendance_weekday_500.txt"):
     try:
